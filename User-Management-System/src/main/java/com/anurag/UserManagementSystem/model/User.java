eb962b1efafd0a5ae2e3161c55ac29969b4e7872
Anurag Shukla <anurag.shukla0206@gmail.com>
2023-06-23 21:03:34 +0530

    Created User-Management-System

---
+++ User-Management-System/src/main/java/com/anurag/UserManagementSystem/model/User.java
@@ -0,0 +1,32 @@
+package com.anurag.UserManagementSystem.model;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+
+    private Integer userId;
+    private String name;
+
+
+    private String userName;
+
+
+    private String userContact;
+
+
+    private String userAddress;
+
+
+
+
+}
---
+++ User-Management-System/src/main/java/com/anurag/UserManagementSystem/repository/UserRepo.java
@@ -0,0 +1,23 @@
+package com.anurag.UserManagementSystem.repository;
+
+
+import com.anurag.UserManagementSystem.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public class UserRepo {
+    @Autowired
+    private  List<User> userList;
+    public List<User> getUsers() {
+        return userList;
+    }
+
+
+    public void delete(User user) {
+        userList.remove(user);
+    }
+}
